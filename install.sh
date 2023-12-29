d94e25d4c6260ea03dcb09253776ae3f5cdd2658
xxf185 <109097973+xxf185@users.noreply.github.com>
2023-12-29 13:20:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -151,7 +151,7 @@ install_x-ui() {
     systemctl daemon-reload
     systemctl enable x-ui
     systemctl start x-ui
-    echo -e "${green}x-ui v${last_version}${plain} 安装完成，面板已启动，"
+    echo -e "${green}x-ui ${last_version}${plain} 安装完成，面板已启动，"
     echo -e ""
     echo -e "x-ui 管理脚本使用方法: "
     echo -e "----------------------------------------------"
