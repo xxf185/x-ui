4742d930725a2868732bff48a9832eb96e658ccc
xxf185 <109097973+xxf185@users.noreply.github.com>
2026-07-01 04:50:22 +0800

    更新 x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -674,7 +674,7 @@ show_log() {
 }
 
 bbr_menu() {
-    echo -e "${green}\t1.${plain} Enable BBR"
+    echo -e "${green}\t1.${plain} 启用 bbr"
     echo -e "${green}\t2.${plain} Disable BBR"
     echo -e "${green}\t0.${plain} Back to Main Menu"
     read -rp "Choose an option: " choice
